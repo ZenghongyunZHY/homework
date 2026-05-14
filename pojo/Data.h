1a79eac11aec56dd6c33230cc93512170d8ad655
zheng hongyun <1911497028@qq.com>
2026-05-14 21:00:41 +0800

    这次引入了确定框架对函数进行了一些解耦

--- pojo/Data.h
+++ pojo/Data.h
@@ -1,20 +1,22 @@
 //
 // Created by Chen on 2026/5/14.
 //
-#include <stdio.h>
+
 #ifndef HOMEWORK_ST_DATA_H
 #define HOMEWORK_ST_DATA_H
+
+#include <stdio.h>
+
 typedef struct {
-    double temp;//温度
-    double salinity;//盐度
-    double ph;//酸碱度
-    double do_value;//溶解氧
-    double precipitation;//降水量
-    double air_temp;//空气温度
+    double temp;
+    double salinity;
+    double ph;
+    double do_value;
+    double precipitation;
+    double air_temp;
 } Data;
 
-//利用链表来实现队列，front指向队头，rear指向队尾,queue指向存储数据的数组
-typedef struct Node{
+typedef struct Node {
     Data* data;
     struct Node* next_data;
     struct Node* prev_data;
@@ -27,12 +29,14 @@ typedef struct {
 } Queue;
 
 typedef struct {
-    FILE* cin_file;//输入文件名
-    FILE* cout_file;//结果文件名
-    Node* prev_read_address;//上次读取的数据地址(队列中的地址)
-    Data* cin_data;//输入数据
-    Data* temp_data;//处理后的数据
-    Data* cout_data;//结果数据
-    Queue* data_queue;//数据队列
+    FILE* cin_file;
+    FILE* cout_file;
+    int is_read_finished;
+    Node* prev_read_address;
+    Data* cin_data;
+    Data* temp_data;
+    Data* cout_data;
+    Queue* data_queue;
 } Data_Manager;
-#endif //HOMEWORK_ST_DATA_H
+
+#endif // HOMEWORK_ST_DATA_H
--- util/operation_file.h
+++ util/operation_file.h
@@ -1,45 +1,76 @@
 #ifndef OPERATION_FILE_H
 #define OPERATION_FILE_H
 
-#include <stdio.h>
+#include "../pojo/Data.h"
 
-//定义结构体
-typedef struct {
-    double temp;//温度
-    double salinity;//盐度
-    double ph;//酸碱度
-    double do_value;//溶解氧
-    double precipitation;//降水量
-    double air_temp;//空气温度
-} Data_Struct;
+typedef Data Data_Struct;
 
-//利用链表来实现队列，front指向队头，rear指向队尾,queue指向存储数据的数组
-typedef struct Node{
-    Data_Struct* data;
-    struct Node* next_data;
-    struct Node* prev_data;
-} Node;
+typedef void (*Data_Read_Func)(Data_Manager* manager, int size);
+typedef void (*Data_Process_Func)(Data_Manager* manager);
 
-typedef struct {
-    Node* front;
-    Node* rear;
-    int size;
-} Queue;
-
-typedef struct {
-    FILE* cin_file;//输入文件名
-    FILE* cout_file;//结果文件名
-    Node* prev_read_address;//上次读取的数据地址(队列中的地址)
-    Data_Struct* cin_data;//输入数据
-    Data_Struct* temp_data;//处理后的数据
-    Data_Struct* cout_data;//结果数据
-    Queue* data_queue;//数据队列
-} Data_Manager;
-Node* get_node_at(Queue* q, int index);
+/**
+ * @brief Initialize file handles and runtime data manager state.
+ *
+ * @param manager Data manager to initialize.
+ * @param cin_filename Input CSV file path.
+ * @return Initialized manager on success, NULL on failure.
+ */
 Data_Manager* init_file(Data_Manager* manager, const char* cin_filename);
+
+/**
+ * @brief Create an empty queue for water quality data nodes.
+ *
+ * @return Newly allocated queue on success, NULL on allocation failure.
+ */
 Queue* create_queue(void);
-int read_file(Data_Manager* manager,int* is_finished);
-void process_data(void);
+
+/**
+ * @brief Get the node at the specified queue index.
+ *
+ * @param q Queue to search.
+ * @param index Zero-based node index.
+ * @return Node pointer on success, NULL if the queue or index is invalid.
+ */
+Node* get_node_at(Queue* q, int index);
+
+/**
+ * @brief Read one data record by delegating to a concrete read strategy.
+ *
+ * @param manager Data manager containing file and queue state.
+ * @param size Maximum queue size used by the read strategy.
+ * @param diff_read_func Concrete read strategy callback.
+ */
+void read_file(Data_Manager* manager, int size, Data_Read_Func diff_read_func);
+
+/**
+ * @brief Process current data by delegating to a concrete process strategy.
+ *
+ * @param manager Data manager containing queue and output state.
+ * @param diff_process_func Concrete process strategy callback.
+ */
+void process_data(Data_Manager* manager, Data_Process_Func diff_process_func);
+
+/**
+ * @brief Write the current output data record to the result file.
+ *
+ * @param manager Data manager containing output file and output data.
+ * @return 1 on success, 0 on invalid state or write failure.
+ */
 int write_file(Data_Manager* manager);
+
+/**
+ * @brief Default read strategy for reading one CSV record into the queue.
+ *
+ * @param manager Data manager containing input file and queue state.
+ * @param size Maximum queue size.
+ */
+void prev_read_func(Data_Manager* manager, int size);
+
+/**
+ * @brief Default process strategy for selecting the output data record.
+ *
+ * @param manager Data manager containing queue and output state.
+ */
+void prev_process_func(Data_Manager* manager);
 
 #endif // OPERATION_FILE_H
